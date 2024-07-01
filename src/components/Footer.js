4c27f891d01d76700c2e954afd78748c7f501490
bkness <kbrandon863@gmail.com>
2024-07-01 14:45:19 -0700

    Add Footer component with subscription form and navigation links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Button } from './Button'
-
+import React from 'react';
+import { Button } from './Button';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
@@ -14,12 +15,54 @@ function Footer() {
                 </p>
                 <div className='input-areas'>
                     <form>
-                        <input type='email' name='email' placeholder='Your Email' className='footer-input' />
+                        <input
+                            type='email'
+                            name='email'
+                            placeholder='Your Email'
+                            className='footer-input'
+                        />
                         <Button buttonStyle='btn--outline'>Subscribe</Button>
                     </form>
                 </div>
             </section>
-
+            <footer className='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonoials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonoials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonoials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonoials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
