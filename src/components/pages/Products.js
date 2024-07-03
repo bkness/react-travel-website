20088faab78c9572097e8fd64ada6d020588aa76
bkness <kbrandon863@gmail.com>
2024-07-03 14:15:28 -0700

    Imported footer to all other pages, bringing consistency throughout the app

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../../App.css'
+import Footer from '../Footer'
 
-export default function Products() {
-    return <h1 className='products'>PRODUCTS</h1>
+function Products() {
+    return (
+        <>
+            <h1 className='products'>PRODUCTS</h1>
+            <Footer />
+        </>
+    )
 }
 
-
+export default Products;
